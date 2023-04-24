f804f31542879d9db3e59e4b47dcc48168b7d068
shaohanhan <shaohanhan00@gmail.com>
2023-04-24 17:43:58 +0800

    取得資料成功

--- app/src/main/java/com/thishkt/pharmacydemo/MainActivity.kt
+++ app/src/main/java/com/thishkt/pharmacydemo/MainActivity.kt
@@ -1,39 +1,47 @@
 package com.thishkt.pharmacydemo
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getPharmaciesData()
+
+        getPharmacyData()
+
     }
 
-    private fun getPharmaciesData() {
+    private fun getPharmacyData() {
         //口罩資料網址
         val pharmaciesDataUrl =
-            "https://raw.githubusercontent.com/thishkt/pharmacies/master/data/info.json"
-//        https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json
+            "https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json"
+//            "https://raw.githubusercontent.com/thishkt/pharmacies/master/data/info.json"
 
         //Part 1: 宣告 OkHttpClient
-        val okHttpClient:OkHttpClient = OkHttpClient().newBuilder().build()
+        val okHttpClient = OkHttpClient().newBuilder().build()
 
         //Part 2: 宣告 Request，要求要連到指定網址
-        val request:Request = Request.Builder().url(pharmaciesDataUrl).get().build()
+        val request: Request = Request.Builder().url(pharmaciesDataUrl).get().build()
 
         //Part 3: 宣告 Call
-        val call:Call = okHttpClient.newCall(request)
+        val call = okHttpClient.newCall(request)
 
         //執行 Call 連線後，採用 enqueue 非同步方式，獲取到回應的結果資料
         call.enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
+            override fun onFailure(call: okhttp3.Call, e: java.io.IOException) {
                 Log.e("HKT", "onFailure: $e")
             }
 
-            override fun onResponse(call: Call, response: Response) {
+            override fun onResponse(call: okhttp3.Call, response: Response) {
                 Log.d("HKT", "onResponse: ${response.body?.string()}")
             }
         })
+
     }
 }
