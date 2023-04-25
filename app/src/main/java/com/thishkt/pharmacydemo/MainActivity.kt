efd33c64757954f31a8decf0aa2b740d93a9af3c
shaohanhan <shaohanhan00@gmail.com>
2023-04-25 16:35:04 +0800

    viewBinding 將資料顯示畫面

--- app/src/main/java/com/thishkt/pharmacydemo/MainActivity.kt
+++ app/src/main/java/com/thishkt/pharmacydemo/MainActivity.kt
@@ -3,15 +3,20 @@ package com.thishkt.pharmacydemo
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.thishkt.pharmacydemo.databinding.ActivityMainBinding
 import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         getPharmacyData()
 
@@ -35,11 +40,17 @@ class MainActivity : AppCompatActivity() {
         //執行 Call 連線後，採用 enqueue 非同步方式，獲取到回應的結果資料
         call.enqueue(object : Callback {
             override fun onFailure(call: okhttp3.Call, e: java.io.IOException) {
-                Log.e("HKT", "onFailure: $e")
+                Log.d("HKT", "onFailure: $e")
             }
 
             override fun onResponse(call: okhttp3.Call, response: Response) {
-                Log.d("HKT", "onResponse: ${response.body?.string()}")
+                val pharmaciesData : String? = response.body?.string()
+//                Log.d("HKT", "onResponse: pharmaciesData")
+
+                runOnUiThread{
+                    //將 Okhttp 獲取到的回應值，指定到畫面的 TextView 元件中
+                    binding.tvPharmaciesData.text= pharmaciesData
+                }
             }
         })
 
